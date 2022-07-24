6efa478dff9fbedefd7dbdccae5229a86c3538f1
Anaid Candido <anaid.candido@gmail.com>
2022-07-24 12:11:19 +0100

    run yarn format

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,10 +1,10 @@
 function AppHeader(): JSX.Element {
-    return (
-      <header>
-        <h1>The places I have found beauty</h1>
-        <small>by Anaid Candido</small>
-      </header>
-    );
-  }
-  
-  export default AppHeader;
+  return (
+    <header>
+      <h1>The places I have found beauty</h1>
+      <small>by Anaid Candido</small>
+    </header>
+  );
+}
+
+export default AppHeader;
