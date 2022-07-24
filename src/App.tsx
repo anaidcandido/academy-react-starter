21dd907766c49f6052d713de3c125d69455dd959
Anaid Candido <anaid.candido@gmail.com>
2022-07-24 12:10:24 +0100

    commented unused JSX elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { greet } from "./utils/greet";
+//import { greet } from "./utils/greet";
+//import AppHeader from "./components/AppHeader";
+//import MainContent from "./components/MainContent";
+import FizzBuzz from "./components/FizzBuzz";
+
+/*function App(): JSX.Element {
+  return (
+    <>
+      <AppHeader />
+      <MainContent />
+      <h1>{greet("World")}</h1>;
+    </>
+  );
+}
+ */
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <>
+      <FizzBuzz />
+    </>
+  );
 }
-
 export default App;
