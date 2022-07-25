9e948aaaf4206472dd34251b05febd9873c562e0
Anaid Candido <anaid.candido@gmail.com>
2022-07-25 10:19:37 +0100

    format code

--- src/components/PlacesInfo.tsx
+++ src/components/PlacesInfo.tsx
@@ -1,24 +1,24 @@
 interface PlaceInfoProps {
-    title: string;
-    name: string;
-    country: string;
-    image: string;
-    mapsLocation: string;
-    reason: string;
-  }
-  
-  function PlacesInfo(props: PlaceInfoProps): JSX.Element {
-    return (
-      <main>
-        <img src={props.image} alt="Edinburgh Castle" />
-        <h2>{props.title}</h2>
-        <h3>
-          {props.name}, {props.country} (
-          <a href={props.mapsLocation}>maps link</a> )
-        </h3>
-        <p>{props.reason}</p>
-      </main>
-    );
-  }
-  
-  export default PlacesInfo;
+  title: string;
+  name: string;
+  country: string;
+  image: string;
+  mapsLocation: string;
+  reason: string;
+}
+
+function PlacesInfo(props: PlaceInfoProps): JSX.Element {
+  return (
+    <main>
+      <img src={props.image} alt="Edinburgh Castle" />
+      <h2>{props.title}</h2>
+      <h3>
+        {props.name}, {props.country} (
+        <a href={props.mapsLocation}>maps link</a> )
+      </h3>
+      <p>{props.reason}</p>
+    </main>
+  );
+}
+
+export default PlacesInfo;
