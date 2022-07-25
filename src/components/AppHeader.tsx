19125a470bd96c125ca9bef66cf0df62b1faec4b
Anaid Candido <anaid.candido@gmail.com>
2022-07-25 10:12:05 +0100

    initial commit

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,10 @@
+function AppHeader(): JSX.Element {
+    return (
+      <header>
+        <h1>The places I have found beauty</h1>
+        <small>by Anaid Candido</small>
+      </header>
+    );
+  }
+  
+  export default AppHeader;
